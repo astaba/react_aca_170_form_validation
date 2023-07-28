3ce4877ef336e0db83f221fc4c27ac2681242285
Astar <bahouidi99@gmail.com>
2023-07-28 17:32:13 +0100

    Complete section 17

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,106 @@
+import React from "react";
+import useFormInput from "../hooks/useFormInput";
+
+const INITIAL_VALUE = {
+  email: "",
+  lastName: "",
+  firstName: "",
+};
+
+const VALIDATIONS = {
+  email: [
+    {
+      isValid: (value) => !!value,
+      message: "Email is required",
+    },
+    {
+      isValid: (value) => /\S+@\S+\.\S+/.test(value),
+      message: "Please enter a valid email",
+    },
+  ],
+  firstName: [
+    {
+      isValid: (value) => !!value,
+      message: "Your first name is required",
+    },
+    {
+      isValid: (value) => /^[A-Za-z\s-]{3,10}$/.test(value),
+      // isValid: (value) => /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/.test(value),
+      message: "Please enter 3 to 1O characters",
+    },
+  ],
+  lastName: [
+    {
+      isValid: (value) => !!value,
+      message: "Your last name is required",
+    },
+    {
+      isValid: (value) => /^[A-Za-z-]{3,10}$/.test(value),
+      message: "Please enter 3 to 1O characters",
+    },
+  ],
+};
+
 const BasicForm = (props) => {
+  const {
+    formInputs,
+    hasError,
+    isDirtyWithError,
+    getErrorMessage,
+    handleChange,
+    handleBlur,
+    smudgeInputs,
+    reset,
+  } = useFormInput(INITIAL_VALUE, VALIDATIONS);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    smudgeInputs();
+    if(hasError) return;
+    const { firstName, lastName, email} = formInputs;
+    alert(`${firstName} ${lastName}\n${email}`);
+    reset();
+  }
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={handleSubmit}>
+      <div className="control-group">
+        <div className={isDirtyWithError("firstName") ? "form-control invalid" : "form-control"}>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            id="firstName"
+            value={formInputs.firstName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        {isDirtyWithError("firstName") ? <p className="error-text">{getErrorMessage("firstName")}</p> : null}
+        <div className={isDirtyWithError("lastName") ? "form-control invalid" : "form-control"}>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            id="lastName"
+            value={formInputs.lastName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
         </div>
+        {isDirtyWithError("lastName") ? <p className="error-text">{getErrorMessage("lastName")}</p> : null}
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={isDirtyWithError("email") ? "form-control invalid" : "form-control"}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          type="text"
+          id="email"
+          value={formInputs.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      {isDirtyWithError("email") ? <p className="error-text">{getErrorMessage("email")}</p> : null}
+      <div className="form-actions">
+        <button disabled={hasError}>Submit</button>
       </div>
     </form>
   );
--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,83 @@
-const SimpleInput = (props) => {
+import React from "react";
+import useFormInput from "../hooks/useFormInput";
+
+const INITIAL_VALUE = {
+  name: "",
+  email: "",
+};
+
+const VALIDATIONS = {
+  email: [
+    {
+      isValid: (value) => !!value,
+      message: "Email is required",
+    },
+    {
+      isValid: (value) => /\S+@\S+\.\S+/.test(value),
+      message: "Please enter a valid email",
+    },
+  ],
+  name: [
+    {
+      isValid: (value) => !!value,
+      message: "Your name is required",
+    },
+    {
+      isValid: (value) => /^\w{3,10}$/.test(value),
+      message: "Please enter a valid name with 3 to 1O characters",
+    },
+  ],
+};
+
+const SimpleInput = () => {
+  const {
+    formInputs,
+    hasError,
+    isDirtyWithError,
+    getErrorMessage,
+    handleChange,
+    handleBlur,
+    smudgeInputs,
+    reset,
+  } = useFormInput(INITIAL_VALUE, VALIDATIONS);
+
+ const handleSubmit = (e) => {
+  e.preventDefault();
+  smudgeInputs();
+  if(hasError) return;
+  const { name, email } = formInputs;
+  alert(name + " - " + email);
+  reset();
+ }
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={handleSubmit}>
+      <div className={isDirtyWithError("name") ? "form-control invalid": "form-control"}>
+        <label htmlFor="name">Your name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formInputs.name}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
       </div>
+      {isDirtyWithError("name") ? <p className="error-text">{getErrorMessage("name")}</p> : null}
+      <div className={isDirtyWithError("email") ? "form-control invalid": "form-control"}>
+        <label htmlFor="email">Your email address:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={formInputs.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+      </div>
+      {isDirtyWithError("email") ? <p className="error-text">{getErrorMessage("email")}</p> : null}
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={hasError}>Save</button>
       </div>
     </form>
   );
